5b29d5693e09b5f9226516207483944747cb0294
K-KY <kyuyoungk@naver.com>
2024-09-23 14:33:50 +0900

    Feat : 다운로드 받은 압축파일 압축 해제

--- src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/corpcode/CorpCodeService.java
+++ src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/corpcode/CorpCodeService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 
 import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Component
 public class CorpCodeService {
@@ -22,7 +24,46 @@ public class CorpCodeService {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            extractCorpCode(file.getPath(), file.getParent());
         }
 
     }
+
+    private void extractCorpCode(String filePath, String parentPath) {
+        try {
+            unzip(filePath, parentPath);
+            System.out.println("압축 해제 완료!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void unzip(String zipFilePath, String parentPath) throws IOException {
+
+        // ZIP 파일을 읽기 위한 스트림 생성
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry zipEntry = zis.getNextEntry();
+
+        // ZIP 파일의 각 엔트리(파일/디렉토리)를 순회하며 압축 해제
+        while (zipEntry != null) {
+            String filePath = parentPath + File.separator + zipEntry.getName();
+            extractFile(zis, filePath);
+            zis.closeEntry();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.close();
+    }
+
+    // 파일을 압축 해제하는 메서드
+    private static void extractFile(ZipInputStream zis, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[4096];
+        int read = 0;
+        while ((read = zis.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+
 }
