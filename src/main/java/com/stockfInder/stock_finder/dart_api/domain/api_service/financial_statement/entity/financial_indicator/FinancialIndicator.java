11b0e79aaf8f315a859acd03e1665739260ec52d
K-KY <kyuyoungk@naver.com>
2024-09-30 02:47:46 +0900

    Feat : 복합키를 사용하지 않도록 변경

--- src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/financial_statement/entity/financial_indicator/FinancialIndicator.java
+++ src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/financial_statement/entity/financial_indicator/FinancialIndicator.java
@@ -1,8 +1,7 @@
 package com.stockfInder.stock_finder.dart_api.domain.api_service.financial_statement.entity.financial_indicator;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
+import com.stockfInder.stock_finder.dart_api.domain.api_service.financial_statement.entity.financial_statement.FinancialStatement;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +13,21 @@ import lombok.ToString;
 @Getter
 @ToString
 public class FinancialIndicator {
-    @EmbeddedId
-    private FinancialIndicatorEmbeddable financialStatementEmbed;//대체키 + FinancialStatement
+    @Id
+    @Column(name = "fin_idc_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int financialIndicatorId;
+
     @Column(name = "idc_code")
     private String indicatorCode;
+
     @Column(name = "idc_name")
     private String indicatorName;
+
     @Column(name = "idc_value")
     private Double indicatorValue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fin_stmt_id")
+    private FinancialStatement financialStatement;
 }
