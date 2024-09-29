b67bb31973eb24ba40d3bb438e8951d5ad4cab4a
K-KY <kyuyoungk@naver.com>
2024-09-30 02:47:46 +0900

    Feat : 복합키를 사용하지 않도록 변경

--- src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/financial_statement/entity/financial_statement/FinancialStatement.java
+++ src/main/java/com/stockfInder/stock_finder/dart_api/domain/api_service/financial_statement/entity/financial_statement/FinancialStatement.java
@@ -1,5 +1,6 @@
 package com.stockfInder.stock_finder.dart_api.domain.api_service.financial_statement.entity.financial_statement;
 
+import com.stockfInder.stock_finder.dart_api.domain.api_service.corpcode.entity.CorpCode;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,16 +15,28 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @ToString
 public class FinancialStatement {
-    @EmbeddedId
-    private FinancialStatementEmbeddable financialStatementEmbeddable;//id 대체키 + corpCode
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fin_stmt_id")
+    private Long financialStatementId;
+
     @Column(name = "report_cpde")
-    private String reportCode;//분기 코드
+    private String reportCode;
+    //분기 코드
     @Column(name = "business_year")
-    private LocalDate businessYear;//사업 년도
+    private LocalDate businessYear;
+    //사업 년도
     @Column(name = "stock_code")
-    private String stockCode;//주식 코드
+    private String stockCode;
+    //주식 코드
     @Column(name = "idc_class_code")
     private String indicatorClassificationCode;
+
     @Column(name = "idc_class_name")
     private String classificationName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "corp_code")
+    private CorpCode corpCode;
+
     }
